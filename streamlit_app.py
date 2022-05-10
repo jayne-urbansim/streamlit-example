b7c7c25fbf7559520d34c0f294d1d5f6ea845412
jayne-urbansim <102245748+jayne-urbansim@users.noreply.github.com>
2022-05-10 11:11:37 -0700

    more containers

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,25 @@ with st.container():
           st.header("An owl")
           st.image("https://static.streamlit.io/examples/owl.jpg")
 
+with st.container():
+    st.write("This is inside the container")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+          st.header("A cat")
+          st.image("https://static.streamlit.io/examples/cat.jpg")
+
+    with col2:
+          st.header("A dog")
+          st.image("https://static.streamlit.io/examples/dog.jpg")
+
+    with col3:
+          st.header("An owl")
+          st.image("https://static.streamlit.io/examples/owl.jpg")
+
+          
+          
 st.write("This is outside the container")
 
 # Sample code from the demo
