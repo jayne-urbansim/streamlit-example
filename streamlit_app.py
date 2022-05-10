d8288f1dcf0ead4f90f3379671c26a13e8c3a1d7
jayne-urbansim <102245748+jayne-urbansim@users.noreply.github.com>
2022-05-10 13:34:04 -0700

    adjusting styles

--- streamlit_app.py
+++ streamlit_app.py
@@ -177,9 +177,9 @@ selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'],
     icons=['house', 'cloud-upload', "list-task", 'gear'], 
     menu_icon="cast", default_index=0, orientation="horizontal",
     styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
+        "container": {"padding": "0!important", "background-color": "#ffffff"},
+        "icon": {"color": "#6941C6", "font-size": "25px"}, 
         "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"},
+        "nav-link-selected": {"background-color": "#DAD9DE"},
     }
 )
