72f114702d34e5563cf9f1819a4e3a8f16d3736b
jayne-urbansim <102245748+jayne-urbansim@users.noreply.github.com>
2022-05-10 13:24:29 -0700

    indents

--- streamlit_app.py
+++ streamlit_app.py
@@ -163,12 +163,12 @@ if st.button("Download"):
 # 1. as sidebar menu
 with st.sidebar:
     selected = option_menu(None, ["Explore Maps",'Scenarios','Scenario Runs','Visualizations'], 
-        icons=['geo-alt', 'pencil-square', 'card-checklist', 'graph-up-arrow'], menu_icon="building", default_index=1)
-        styles={
-             "container": {"padding": "0!important", "background-color": "#fafafa"},
-             "icon": {"color": "orange", "font-size": "25px"}, 
-             "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-             "nav-link-selected": {"background-color": "green"},
+     icons=['geo-alt', 'pencil-square', 'card-checklist', 'graph-up-arrow'], menu_icon="building", default_index=1)
+     styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
     selected
 
 # 2. horizontal menu
