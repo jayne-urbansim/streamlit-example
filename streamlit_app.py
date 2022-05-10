33c104a2b304213e7b31e655aa443e053c9e5ca4
jayne-urbansim <102245748+jayne-urbansim@users.noreply.github.com>
2022-05-10 10:56:55 -0700

    Button
    try adding a button

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,10 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
+if st.button('Say hello'):
+     st.write('Why hello there')
+ else:
+     st.write('Goodbye')
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
