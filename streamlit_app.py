13887b58f12cd14b09a9d738dbd9f6555a765a44
jayne-urbansim <102245748+jayne-urbansim@users.noreply.github.com>
2022-05-10 13:52:48 -0700

    Adjust Side Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,16 +48,16 @@ with st.sidebar:
      
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
+    "Region",
+    ("Vancouver", "Toronto", "Calgary")
 )
 
 # Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )     
+#with st.sidebar:
+#    add_radio = st.radio(
+#        "Choose a shipping method",
+#        ("Standard (5-15 days)", "Express (2-5 days)")
+#   )     
 
 # ----- Container Stuff -----
 
