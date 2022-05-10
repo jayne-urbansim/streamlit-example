48c6d4676c1600a7a92761fb5435d5250d1ad708
jayne-urbansim <102245748+jayne-urbansim@users.noreply.github.com>
2022-05-10 12:19:16 -0700

    option menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,9 @@ import requests
 from streamlit_lottie import st_lottie
 from streamlit_lottie import st_lottie_spinner
 
+from streamlit_option_menu import option_menu
+
+
 #import pandas as pd
 
 # ---- Page Configuration -----
@@ -154,3 +157,29 @@ if st.button("Download"):
     with st_lottie_spinner(lottie_download, key="download"):
         time.sleep(5)
     st.balloons()
+
+# Option menu
+
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+# 2. horizontal menu
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
+
+# 3. CSS style definitions
+selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
