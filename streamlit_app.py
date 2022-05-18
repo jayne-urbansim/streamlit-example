83e2958dbbfac87a6933f37dfd929ccdecf46f34
Jayne Vidheecharoen <102245748+jayne-urbansim@users.noreply.github.com>
2022-05-18 15:56:32 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,12 +133,11 @@ st.write("This is outside the container")
 # ----- Expander Stuff -----
 
 with st.expander("See explanation"):
-	st.checkbox('this is an optional thing')
-	st.checkbox('this is an optional thing')
+    st.checkbox('this is an optional thing')
+    st.checkbox('this is an optional thing')
 	
 # Sample code from the demo
-    
-     with st.echo(code_location='below'):
+    with st.echo(code_location='below'):
          total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
          num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
