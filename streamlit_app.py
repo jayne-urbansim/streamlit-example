2b387020e8177b41331ebe12d5292623326fde25
Jayne Vidheecharoen <102245748+jayne-urbansim@users.noreply.github.com>
2022-08-01 14:33:52 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,18 @@ add_selectbox = st.sidebar.selectbox(
 
 # ---- Tab test ---
 
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+with tab1:
+	st.header("A cat")
+	st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
+with tab2:
+	 st.header("A dog")
+	 st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+	st.header("An owl")
+	st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 # ----- Container Stuff -----
 
@@ -110,23 +121,9 @@ with st.container():
 
     col1, col2, col3 = st.columns(3)
 
-    with col1:
-		
-	tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-
-   	with tab1:
-   		st.header("A cat")
-    		st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-
-	with tab2:
-   		 st.header("A dog")
-  		 st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-	with tab3:
- 		st.header("An owl")
-  		st.image("https://static.streamlit.io/examples/owl.jpg", width=200)	
+    with col1:	
 	st.subheader("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
+       	st.image("https://static.streamlit.io/examples/cat.jpg")
 
     with col2:
 	st.subheader("A dog")
